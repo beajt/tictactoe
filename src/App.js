f7673100d18d2ee7548b1039eea93fb6c5e177cf
beatuano <beatuano@gmail.com>
2023-03-27 21:16:25 -0400

    added media queries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Game from './Components/Game';
+import Header from './Header';
 import DarkMode from './Components/DarkMode';
+import Game from './Components/Game';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <DarkMode />
       <Game />
     </div>
